7abf0f59caefb524ffaebd3d4bed3121802dc815
cxoltero <carlosh95@gmail.com>
2014-11-03 21:36:56 -0500

    added contoller index and new views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,30 @@
 class ArticlesController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+
+  end
+
+  def edit
+  end
+
+  private
+  def article_params
+      params.require(:article).permit(:title, :content)
+  end
+
+
 end
